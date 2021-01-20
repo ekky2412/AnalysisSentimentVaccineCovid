21326eea873da021d9e55e3a49ec35062ac549d8
WillianKelvin <williankvn2008@gmail.com>
2021-01-20 09:12:51 +0700

    first commit

---
+++ VaksinCovidShiny.R
@@ -0,0 +1,110 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(vroom)
+library(here)
+library(dplyr)
+library(ggplot2)
+library(RColorBrewer)
+library(SnowballC)
+library(Rstem)
+library(sentiment)
+library(plyr)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  headerPanel("Analisis Sentimen Vaksin Covid-19"),
+  headerPanel("Menggunakan Algoritma Naive Bayes"),
+            mainPanel(
+                tabsetPanel(
+                    tabPanel("Data Twitter", DT::dataTableOutput('dataTwitter')),
+                    tabPanel("Data Cleaned", DT::dataTableOutput('dataCleaned')),
+                    tabPanel("Data Sentimen", DT::dataTableOutput('tbl')),
+                    tabPanel("Plot Tweet", plotOutput("sent2"))
+                )
+            )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    dataTwitter<- vroom(here("D://Kuliah//SEMESTER 5//Data Science/Proyek//dataTwitter.csv"))
+    dataTwitter<- data.frame(dataTwitter)
+    # Output Data
+    output$dataTwitter = DT::renderDataTable({
+      DT::datatable(dataTwitter, options = list(lengthChange = FALSE))
+    })
+    
+    sent_df<- vroom(here("D://Kuliah//SEMESTER 5//Data Science/Proyek//dataSentimen.csv"))
+    sent_df <- data.frame(sent_df)
+    dataCleaned<- vroom(here("D://Kuliah//SEMESTER 5//Data Science/Proyek//dataCleaned.csv"))
+    dataCleaned<- data.frame(dataCleaned)
+    # Output Data
+    output$dataCleaned = DT::renderDataTable({
+      DT::datatable(dataCleaned, options = list(lengthChange = FALSE))
+    })
+  
+    sent_df<- vroom(here("D://Kuliah//SEMESTER 5//Data Science/Proyek//dataSentimen.csv"))
+    sent_df <- data.frame(sent_df)
+    # Output Data
+    output$tbl = DT::renderDataTable({
+      DT::datatable(sent_df, options = list(lengthChange = FALSE))
+    })
+    
+    # plot distribution of emotions
+    ggplot(sent_df, aes(x=emotion)) +
+      geom_bar(aes(y=..count.., fill=emotion)) +
+      scale_fill_brewer(palette="Dark2") +
+      labs(x="emotion categories", y="number of tweets") +
+      labs(title = "Sentiment Analysis of Vaccine Covid-19",
+           plot.title = element_text(size=12))
+    plotSentiments1 <- function(sentiment_dataframe, title) 
+    {
+      library(ggplot2)
+      ggplot(sentiment_dataframe, aes(x=emotion)) + 
+        geom_bar(aes(y=..count.., fill=emotion)) + 
+        scale_fill_brewer(palette="Dark2") + 
+        ggtitle(title) + 
+        theme(legend.position="right") + 
+        ylab("Number of Tweets") + 
+        xlab("Emotion Categories")
+    }
+    #plotting tweets emotions
+    output$sent1 <- renderPlot({
+      plotSentiments1(sent_df, "Sentiment Analysis of Vaccine Covid-19")
+    })
+    
+    # plot distribution of polarity
+    ggplot(sent_df, aes(x=polarity)) +
+      geom_bar(aes(y=..count.., fill=polarity)) +
+      scale_fill_brewer(palette="RdGy") +
+      labs(x="polarity categories", y="number of tweets") +
+      labs(title = "Sentiment Analysis of Vaccine Covid-19",
+           plot.title = element_text(size=12))
+    plotSentiments2 <- function(sent_df, title)
+    {
+      library(ggplot2)
+      ggplot(sent_df, aes(x=polarity)) +
+        geom_bar(aes(y=..count.., fill=polarity)) +
+        scale_fill_brewer(palette="RdGy") +
+        ggtitle(title) +
+        theme(legend.position="right") +
+        ylab("Number of Tweets") +
+        xlab("Polarity Categories")
+    }
+    
+    output$sent2 <- renderPlot({
+      plotSentiments2(sent_df, "Sentiment Analysis of Vaccine Covid-19")
+    })
+    
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ VaksinCovidTwitter.R
@@ -0,0 +1,139 @@
+library(twitteR)
+library(RCurl)
+library(wordcloud)
+library(corpus)
+library(tm)
+library(SnowballC)
+library(Rstem)
+library(twitteR)
+library(tm)
+library(NLP)
+library(SentimentAnalysis)
+library(plyr)
+library(ggplot2)
+library(RColorBrewer)
+library(wordcloud)
+library(sentiment)
+
+consumer_key = "KKWw2ERsKER8njU5SiohACat7";
+consumer_secret = "GRFzLOd0v0lEB12hQ3UHngxHT11PdIYa0mn8t14PqmhRQpQNtc";
+bearer_token = "AAAAAAAAAAAAAAAAAAAAANsjKAEAAAAA11ksFen0WsuiW3orxPBJGd1siVc%3Drr2QbmUjl9ynmxEfTWY5U0UlCOY4cF0mgM28L1U2IRFmLdPzDN";
+access_token = "488398717-OeYDNC8ed1ZIwppCdlqllGxOA39RFhNKhOOhEab1";
+access_secret = "47nLd2g4MbSy2t5HIG7ZOxTYLhFgdhZBqpk2MAUyuV7KM";
+setup_twitter_oauth(consumer_key,consumer_secret,access_token,access_secret);
+
+#mining data twitter
+miningtweets <- searchTwitter('Vaccine COVID',lang="en",n=2000, resultType = "mixed");
+miningtweets_text <- sapply(miningtweets,function(x) x$getText());
+write.csv(miningtweets_text, paste('D:\\Kuliah\\SEMESTER 5\\Data Science\\Proyek\\','dataTwitter.csv',sep = ''))
+str(miningtweets_text);
+
+#BB_corpus = Corpus(VectorSource(miningtweets_text))
+#inspect(BB_corpus)
+
+# remove retweet entities
+miningtweets_text = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", " ", miningtweets_text)
+# remove at people
+miningtweets_text = gsub("@\\w+", " ", miningtweets_text)
+# remove punctuation
+miningtweets_text = gsub("[[:punct:]]", " ", miningtweets_text)
+# remove numbers
+miningtweets_text = gsub("[[:digit:]]", " ", miningtweets_text)
+# remove html links
+miningtweets_text = gsub("http\\w+", " ", miningtweets_text)
+# remove unnecessary spaces
+miningtweets_text = gsub("[ \t]{2,}", " ", miningtweets_text)
+miningtweets_text = gsub("^\\s+|\\s+$", " ", miningtweets_text)
+miningtweets_text = gsub("note", " ", miningtweets_text)
+miningtweets_text = gsub("\n", " ", miningtweets_text)
+miningtweets_text = gsub("[^\x01-\x7F]", " ", miningtweets_text)
+miningtweets_text = gsub("amp", " ", miningtweets_text)
+
+# define "tolower error handling" function 
+try.error = function(x)
+{
+  # create missing value
+  y = NA
+  # tryCatch error
+  try_error = tryCatch(tolower(x), error=function(e) e)
+  # if not an error
+  if (!inherits(try_error, "error"))
+    y = tolower(x)
+  # result
+  return(y)
+}
+
+# lower case using try.error with sapply 
+miningtweets_text = sapply(miningtweets_text, try.error)
+# remove NAs in miningtweets_text
+miningtweets_text = miningtweets_text[!is.na(miningtweets_text)]
+names(miningtweets_text) = NULL
+
+write.csv(miningtweets_text, paste('D:\\Kuliah\\SEMESTER 5\\Data Science\\Proyek\\','dataCleaned.csv',sep = ''))
+
+# classify emotion
+class_emo = classify_emotion(miningtweets_text, algorithm="bayes", prior=1.0)
+# get emotion best fit
+emotion = class_emo[,7]
+# substitute NA's by "unknown"
+emotion[is.na(emotion)] = "unknown"
+# classify polarity
+class_pol = classify_polarity(miningtweets_text, algorithm="bayes")
+# get polarity best fit
+polarity = class_pol[,4]
+View(polarity)
+
+# data frame with results
+sent_df = data.frame(text=miningtweets_text, emotion=emotion,
+                     polarity=polarity, stringsAsFactors=FALSE)
+# sort data frame
+sent_df = within(sent_df,
+                 emotion <- factor(emotion, levels=names(sort(table(emotion), decreasing=TRUE))))
+write.csv(sent_df, paste('D:\\Kuliah\\SEMESTER 5\\Data Science\\Proyek\\','dataSentimen.csv',sep = ''))
+View(sent_df)
+head(sent_df,20)
+table(sent_df$emotion)
+# plot distribution of emotions
+ggplot(sent_df, aes(x=emotion)) +
+  geom_bar(aes(y=..count.., fill=emotion)) +
+  scale_fill_brewer(palette="Dark2") +
+  labs(x="emotion categories", y="number of tweets") +
+  labs(title = "Sentiment Analysis dari Vaksin COVID-19",
+       plot.title = element_text(size=12))
+plotSentiments1 <- function(sentiment_dataframe, title) 
+{
+  library(ggplot2)
+  ggplot(sentiment_dataframe, aes(x=emotion)) + 
+    geom_bar(aes(y=..count.., fill=emotion)) + 
+    scale_fill_brewer(palette="Dark2") + 
+    ggtitle(title) + 
+    theme(legend.position="right") + 
+    ylab("Number of Tweets") + 
+    xlab("Emotion Categories")
+}
+
+#plotting tweets emotions
+plotSentiments1(sent_df, "Sentiment Analysis of Vaccine Covid-19")
+
+
+# plot distribution of polarity
+ggplot(sent_df, aes(x=polarity)) +
+  geom_bar(aes(y=..count.., fill=polarity)) +
+  scale_fill_brewer(palette="RdGy") +
+  labs(x="polarity categories", y="number of tweets") +
+  labs(title = "Sentiment Analysis dari Vaksin COVID-19",
+       plot.title = element_text(size=12))
+plotSentiments2 <- function(sent_df, title)
+{
+  library(ggplot2)
+  ggplot(sent_df, aes(x=polarity)) +
+    geom_bar(aes(y=..count.., fill=polarity)) +
+    scale_fill_brewer(palette="RdGy") +
+    ggtitle(title) +
+    theme(legend.position="right") +
+    ylab("Number of Tweets") +
+    xlab("Polarity Categories")
+}
+
+#plotting tweets polarity
+plotSentiments2(sent_df, "Polarity Analysis of Vaccine Covid-19")
